ed3a58b31ce5d053137e1faaefeaa1fda46a9fee
vischhc <shogatjisatsu@gmail.com>
2026-07-09 13:43:04 +0300

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,1 +1,11 @@
-console.log(courses);
+const app = document.getElementById("app");
+
+app.innerHTML = `
+    <h2>${courses[0].title}</h2>
+
+    <p><b>Урок:</b> ${courses[0].lessons[0].title}</p>
+
+    <p><b>Оценка:</b> ${courses[0].lessons[0].result.score}/12</p>
+
+    <p><b>Навык:</b> ${courses[0].lessons[0].result.skill}</p>
+`;
